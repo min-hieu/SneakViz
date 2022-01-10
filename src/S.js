ae801988f0e375390a04e67b21d645fd6879d694
min-hieu <hieuristics@kaist.ac.kr>
2022-01-10 16:49:54 +0900

    switched to point cloud

--- src/S.js
+++ src/S.js
@@ -1,3 +1,4 @@
+import * as THREE from 'three'
 import { useState } from 'react'
 import { useTexture } from '@react-three/drei'
 import * as d3 from 'd3'
@@ -5,17 +6,14 @@ import XS from './data/test_umap3d_new.csv'
 import M from './data/sampled_color_umap3d.csv'
 import L from './data/sampled_color_umap3d_2.csv'
 import XL from './data/large_test_umap3d.csv'
+import XLrgb from './data/large_test_umap3d_rgb.csv'
+import branded from './data/sampled_brand_umap3d.csv'
+import branded3 from './data/sampled_brand3_umap3d.csv'
+
 import shoe from './img/shoe.jpeg'
 
 const selectCol = "#ff3d7b"
-const shoeSize = L
-
-
-const handleHoverOver = (e) => {
-}
-
-const handleHoverOut = (e,setHover) => {
-}
+const shoeSize = XLrgb
 
 const S = ({ col,pos }) => {
   const [hover, setHover] = useState(false)
@@ -24,8 +22,8 @@ const S = ({ col,pos }) => {
   return (
     <sprite
       position={pos}
-      onPointerOver={(e) => {handleHoverOver(e);setHover(true)}}
-      onPointerOut={(e) => {handleHoverOut(e);setHover(false)}}
+      onPointerOver={(e) => setHover(true)}
+      onPointerOut={(e) => setHover(false)}
       scale={[size,size,size]}
     >
       <spriteMaterial
@@ -35,9 +33,67 @@ const S = ({ col,pos }) => {
   )
 }
 
-const createRandomSprite = ( numSprite ) => {
+
+const makeCloud = async () => {
+  const geometry = new THREE.BufferGeometry();
+  const color = new THREE.Color();
+
+  const positions = [];
+  const colors = [];
+
+  const data = await d3.csv(shoeSize)
+
+  for (let i=0;i<data.length;i++) {
+    let p = data[i]
+    positions.push( p.x, p.y, p.z );
+    colors.push( p.r, p.g, p.b );
+  }
+
+  geometry.setAttribute( 'position', new THREE.Float32BufferAttribute( positions, 3 ) );
+  geometry.setAttribute( 'color', new THREE.Float32BufferAttribute( colors, 3 ) );
+
+  return geometry
+}
+
+
+const makeDemo = () => {
+  const particles = 500000;
+  const geometry = new THREE.BufferGeometry();
+  const color = new THREE.Color();
+
+  const positions = [];
+  const colors = [];
+
+  const n = 1000, n2 = n / 2; // particles spread in the cube
+
+  for ( let i = 0; i < particles; i ++ ) {
+    // positions
+    const x = Math.random() * n - n2;
+    const y = Math.random() * n - n2;
+    const z = Math.random() * n - n2;
+    positions.push( x, y, z );
+    // colors
+    const vx = ( x / n ) + 0.5;
+    const vy = ( y / n ) + 0.5;
+    const vz = ( z / n ) + 0.5;
+    color.setRGB( vx, vy, vz );
+    colors.push( color.r, color.g, color.b );
+  }
+
+  geometry.setAttribute( 'position', new THREE.Float32BufferAttribute( positions, 3 ) );
+  geometry.setAttribute( 'color', new THREE.Float32BufferAttribute( colors, 3 ) );
+
+  return (
+    <points geometry={geometry}>
+      <pointsMaterial size={5} vertexColors={true} />
+    </points>
+  )
+}
+
+
+const createRandomCloud = ( N ) => {
   let sprites = []
-  for (let i = 0; i < numSprite; i++) {
+  for (let i = 0; i < N; i++) {
     let pos = [Math.random()*4-2,Math.random()*4-2,Math.random()*4-2]
     let col = randomColor()
     let key = "s" + i
@@ -48,6 +104,7 @@ const createRandomSprite = ( numSprite ) => {
   return sprites
 }
 
+
 const randomColor = () => {
   var letters = '0123456789ABCDEF';
   var color = '#';
@@ -57,6 +114,7 @@ const randomColor = () => {
   return color;
 }
 
+
 const setupUmap = () => {
   let sprites = []
   let i = 0
@@ -64,17 +122,22 @@ const setupUmap = () => {
     let key = "s" + i
     i += 1
     sprites.push(
-      <S key={key} pos={[point.x,point.y,point.z]} col={point.color}/>
+      <S
+        key={key}
+        pos={[point.x,point.y,point.z]}
+        col={point.color}
+      />
     )
   });
   sprites.push(
     <S key={"center"} pos={[0,0,0]} col={"#0F0"}/>
   )
-  console.log("loading done!")
   return sprites;
 }
 
 export {
-  createRandomSprite,
-  setupUmap
+  createRandomCloud,
+  setupUmap,
+  makeDemo,
+  makeCloud
 }
--- src/screen.js
+++ src/screen.js
@@ -1,7 +1,7 @@
-import React, { useState, Suspense } from 'react'
+import React, { useState, useEffect, useMemo } from 'react'
 import { Canvas } from '@react-three/fiber'
 import { useTexture, OrbitControls, FlyControls, PointerLockControls } from '@react-three/drei'
-import { createRandomSprite, setupUmap } from './S'
+import { createRandomCloud, setupUmap, makeCloud, makeDemo } from './S'
 import frame from './img/frame.png'
 
 const styles = {
@@ -28,12 +28,18 @@ const styles = {
     transform: "translate(-50%, 0)",
     bottom: "5vh",
   },
+  loading: {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%,-50%)",
+  },
 }
 
-const speed = 4
+const speed = 2
 
 const sphere = () => {
-  const radius = 99
+  const radius = 8
   return (
     <mesh>
       <sphereGeometry args={[radius, 8, 6]}/>
@@ -42,47 +48,80 @@ const sphere = () => {
   )
 }
 
+
 const Screen = () => {
-  const setupSprites = setupUmap()
+  const cloudGeo = useMemo(() => makeCloud(), [])
+  const demoSprite = useMemo(() => makeDemo(), [])
+  const slowSprite = useMemo(() => setupUmap(), [])
   const [select, setSelect] = useState()
-  const [sprites, setSprites] = useState(setupSprites)
+  const [plot, setPlot] = useState(null)
+  const [plotDefault, setPlotDefault] = useState(null)
   const [spin, setSpin] = useState(speed);
+  const [loadStatus, setLoadStatus] = useState(null);
+
+  useEffect(() => {
+    cloudGeo.then((geometry) => {
+      setLoadStatus(1)
+      console.log("done loading lol")
+      setPlot(
+        <points geometry={geometry}>
+          <pointsMaterial size={0.1} vertexColors={true} />
+        </points>
+      )
+      setPlotDefault(
+        <points geometry={geometry}>
+          <pointsMaterial size={0.1} vertexColors={true} />
+        </points>
+      )
+    })
+  }, [])
+
+  if (loadStatus === null) {
+    return (
+      <div style={styles.loading}>
+        loading data lol ...
+      </div>
+    )
+  }
 
   return (
     <>
-      <Suspense>
-        <Canvas
-          style={styles.canvas}
-          camera={{
-            fov: 999,
-            position: [0, 0, 6]
-          }}
-        >
-          <OrbitControls
-            autoRotate={true}
-            autoRotateSpeed={spin}
-          />
-          <ambientLight />
-          <pointLight position={[10, 10, 10]} />
-          {sprites}
-          <mesh>
-            <sphereGeometry args={[9999, 8, 6]}/>
-            <meshBasicMaterial color="#FF0" />
-          </mesh>
-        </Canvas>
-      </Suspense>
+      <Canvas
+        linear
+        gl={{ antialias: true, alpha: true }}
+        style={styles.canvas}
+        camera={{
+          fov: 999,
+          position: [0, 0, 7],
+          far: 100000,
+        }}
+      >
+        <OrbitControls
+          autoRotate={true}
+          autoRotateSpeed={spin}
+        />
+        <ambientLight />
+        <pointLight position={[10, 10, 10]} />
+        {plot}
+      </Canvas>
       <img src={frame} style={styles.frame}/>
       <div className="uiControls"
         style={styles.controls}
       >
-        <button onClick={(e) => setSprites(createRandomSprite(12))}>
-          Shuffle
+        <button onClick={(e) => setPlot(createRandomCloud(12))}>
+          random
         </button>
         <button onClick={(e) => setSpin(spin?0:speed)}>
           Spin Switch
         </button>
-        <button onClick={(e) => setSprites(setupSprites)}>
-          reset
+        <button onClick={(e) => setPlot(plotDefault)}>
+          makeCloud
+        </button>
+        <button onClick={(e) => setPlot(demoSprite)}>
+          makeDemo
+        </button>
+        <button onClick={(e) => setPlot(slowSprite)}>
+          old
         </button>
       </div>
     </>

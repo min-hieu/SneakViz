fa64227cb73a2154560f49a5dd54452ae22e94eb
min-hieu <hieuristics@kaist.ac.kr>
2022-01-21 10:02:51 +0900

    stable vr

--- src/screen.js
+++ src/screen.js
@@ -1,36 +1,26 @@
-import React, { useState, useEffect, useMemo } from 'react'
-import { Canvas } from '@react-three/fiber'
+import React, { useState, useEffect, useMemo, useRef } from 'react'
+import { useFrame } from '@react-three/fiber'
 import { OrbitControls } from '@react-three/drei'
-import { MakeCloud2D, MakeCloudTexture } from './datapoint'
-import frame from './img/frame.png'
+import { MakeCloud } from './datapoint'
+import { 
+	VRCanvas,
+	DefaultXRControllers,
+	Hands,
+	useController,
+	Interactive,
+	useXRFrame,
+	RayGrab,
+} from '@react-three/xr'
+import { VRButton } from 'three/examples/jsm/webxr/VRButton'
 
 const styles = {
   canvas: {
     height: "100vh",
     width: "100vw",
-    background: "white",
     position: "absolute",
     left: "50%",
     top: "50%",
     transform: "translate(-50%, -50%)",
-  },
-  frame: {
-    position: "absolute",
-    top: 0,
-    height: "100vh",
-    left: "50%",
-    transform: "translate(-50%, 0)",
-    pointerEvents: "none",
-    display: "None",
-  },
-  controls: {
-    position: "absolute",
-    bottom: "2vh",
-    margin: "102vh 0 2vh 0",
-    width: "100vw",
-    display: "flex",
-    justifyContent: "center",
-    gap: "2vw",
   },
   loading: {
     position: "absolute",
@@ -40,23 +30,59 @@ const styles = {
   },
 }
 
-const speed = 0
+const CloudMesh = ({geo, mat}) => {
+	const ref = useRef()
+	const [pressed, setPressed] = useState(false)
+	const [startSeq, setStartSeq] = useState(0)
+	const rightCon = useController("right")
+	const leftCon = useController("left")
 
-const Screen = () => {
-  const cloudGeo = useMemo(() => MakeCloud2D({shoeType: "label"}), [])
-  const [plot, setPlot] = useState(null)
-  const [plotDefault, setPlotDefault] = useState(null)
-  const [spin, setSpin] = useState(speed);
+	useFrame((state, delta) => {
+		if (pressed) {
+			ref.current.scale.x += 11
+			ref.current.scale.y *= 0.9
+			ref.current.scale.z *= 0.9
+		}
+
+		if (startSeq == 0) {
+			ref.current.scale.x = 0.001
+			ref.current.scale.y = 0.001
+			ref.current.scale.z = 0.001
+			setStartSeq(1)
+		}
+		if (startSeq == 1) {
+			ref.current.scale.x *= 1.1
+			ref.current.scale.y *= 1.1
+			ref.current.scale.z *= 1.1
+			if (ref.current.scale.x > 2) {
+				setStartSeq(2)
+			}
+		}
+	})
+	
+	return (
+		<Interactive
+			onSelectStart={(e) => setPressed(true)}
+			onSelectEnd={() => setPressed(false)} 
+		>
+			<points geometry={geo} material={mat} ref={ref}/>
+		</Interactive>
+	)
+}
+
+export default function Screen() {
+  const cloudData = useMemo(() => MakeCloud({shoeType: "shape"}), [])
+  const [cloud, setCloud] = useState(null)
   const [loadStatus, setLoadStatus] = useState(null);
+	
 
   useEffect(() => {
-    cloudGeo.then((mesh) => {
+    cloudData.then((data) => {
+			setCloud(data)
       setLoadStatus(1)
       console.log("loaded!")
-      setPlot(mesh)
-      setPlotDefault(mesh)
     })
-  }, [])
+  }, [cloudData])
 
   if (loadStatus === null) {
     return (
@@ -64,41 +90,33 @@ const Screen = () => {
         loading data ⌛️
       </div>
     )
-  }
+  } else {
+		return (
+			<>
+				<VRCanvas
+					linear
+					gl={{ antialias: true, alpha: false }}
+					style={styles.canvas}
+					camera={{
+						fov: 999,
+						position: [0, 0, 7],
+						far: 100000,
+					}}
+					mode="concurrent"
+					onCreated={(args) => {
+						args.gl.setClearColor('white')
+						void document.body.appendChild(VRButton.createButton(args.gl))
+					}}
+				>
+					<OrbitControls autoRotate={true} autoRotateSpeed={2}/>
+					<DefaultXRControllers />
+					<Hands />
+					<ambientLight intensity={0.3} />
+					<pointLight position={[0, 5, 5]} intensity={1} />
 
-  return (
-    <>
-      <Canvas
-        linear
-        gl={{ antialias: true, alpha: true }}
-        style={styles.canvas}
-        camera={{
-          fov: 999,
-          position: [0, 0, 7],
-          far: 100000,
-        }}
-      >
-        <OrbitControls
-          autoRotate={true}
-          autoRotateSpeed={spin}
-        />
-        <ambientLight intensity={1}/>
-        <pointLight position={[10, 10, 10]} />
-        {plot}
-      </Canvas>
-      <img src={frame} style={styles.frame}/>
-      <div className="uiControls"
-        style={styles.controls}
-      >
-        <button onClick={(e) => setSpin(spin?0:speed)}>
-          Spin Switch
-        </button>
-        <button onClick={(e) => setPlot(plotDefault)}>
-          makeCloud
-        </button>
-      </div>
-    </>
-  )
+						<CloudMesh geo={ cloud.geo } mat={ cloud.mat }/>
+				</VRCanvas>
+			</>
+		)
+	}
 }
-
-export default Screen

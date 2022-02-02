f62c3d49da2dc7125a43eefa109037103e654edf
min-hieu <hieuristics@kaist.ac.kr>
2022-02-02 15:19:13 +0900

    stable website

--- src/App.js
+++ src/App.js
@@ -1,10 +1,89 @@
 import styles from './styles/App.css'
-import Screen from './screen'
+import { StaticScreen, DynamicScreen } from './screen'
+import {
+	FaGithubSquare,
+} from 'react-icons/fa'
+import { AiFillGoogleSquare } from 'react-icons/ai'
+import shoeGif from './img/shoe.gif'
+import ibsLogo from './img/data_science_small_logo.png'
+
+const title =
+	<div className="titleContainer">
+		<div className="titleUpper">
+			<span>22 Years of</span>
+		</div>
+		<div className="titleLower">
+			<span>Sneak</span>
+			<img className="titleGif" src={shoeGif} />
+			<span>rs</span>
+		</div>
+	</div>
+	
+const subtitle =
+	<>
+		<span className="subtitle">
+			Web data and computational models can play an important role in analyzing cultural trends. The current study uses <b>23,492</b> sneaker product images and meta information collected from a global reselling shop, StockX.com. We construct an index named sneaker design index that summarizes the design characteristics of sneakers using a <b>contrastive learning</b> method. This index allows us to study changes in design over a 22 year-long period (1999-2020).
+		</span>
+		<span className="subtitle">
+			original paper: <a>link</a>
+			<br />
+			project's github: <a>link</a>
+		</span>
+	</>
+
+const Section = ({header, contents}) => {
+	let contentList = contents.map((content) => (
+		<span key={content} className="section-content">
+			&emsp;{content}
+		</span>
+	))
+	return (
+		<div className="section">
+			<span className="section-header">
+				{header}
+			</span>
+			{contentList}
+		</div>
+	)
+}
+
+const footer =
+	<div className="footerContainer">
+		<span className="credit"><b>Main Author</b>: Sungkyu Shaun Park (박성규), Ph.D.</span>
+		<span className="credit">
+			<b>Data Visualizer</b>: Nguyen Minh Hieu   
+			<FaGithubSquare /> 
+			<AiFillGoogleSquare />
+		</span>
+		<img src={ibsLogo} className="footerLogo" onClick={()=> window.open("https://ds.ibs.re.kr/")}/>
+	</div>
+
+const text1 = [
+	{ 
+		h: "Background",
+		c: [
+			"The online reselling market is proliferating. One of the fastest- growing sectors is fashion , as noted by Anne-Marie Tomchak, a former Vogue digital director, “there are literally tens of millions of pounds worth of clothes sitting dormant in people’s wardrobes.” Many reselling markets like eBay, Depop, Vinted, and Etsy count tens of millions of users, turning themselves into excellent plat- forms for trading fashion items. Data accumulated within these sites provide a unique opportunity to study how fashion trend has evolved (or sometimes revolved) over a longitudinal period.",
+			"With the use of 22-year spanning Web data, the current research makes the following observations and contributions: 1) Based on feature engineering, we find temporal patterns in design change, e.g., sneakers tend to be more pastel-toned and brighter over time; 2) We present a novel neural-net based embedding jointly considering color and shape information from the given sneaker images; 3) The proposed embedding can infer key cultural trends of sneakers including the product category, target consumer, and high premium in the reselling market; 4) Based on the extracted latent representation, we find that the major sneaker brands follow similar patterns for color whereas they cling to their design patterns for shape over two decades. To the best of our knowledge, these patterns observed via large-scale Web data have never been studied before."
+		]
+	},
+	{
+		h: "Exploring Sneaker Color trend in 3D",
+		c: [
+			"Data suggest that sneakers tend to employ brighter colors and lower hue and saturation values over time. Yet, each brand continues to build toward its particular trajectory of shape-related design patterns. The embedding analysis also predicts which sneakers will likely see a high premium in the reselling market, suggesting a viable algorithm-driven investment and design strategies. The current work is the first to apply data science methods to a new research domain — i.e., analysis of product design evolution over a long his- torical period — and has implications for the novel use of Web data to understand cultural patterns that are otherwise hard to obtain."
+		]
+	}
+]
 
 export default function App() {
   return (
-    <div className={styles.container}>
-      <Screen />
+    <div className="container">
+			{title}
+			{subtitle}
+			<Section header={text1[0].h} contents={text1[0].c}/>
+      <StaticScreen />
+			<Section header={text1[0].h} contents={text1[0].c}/>
+			<DynamicScreen />
+			{footer}
     </div>
   )
 }
--- src/datapoint.js
+++ src/datapoint.js
@@ -5,88 +5,143 @@ import { useFrame, useRef } from 'react'
 import color from './data/color_umap.csv'
 import shape from './data/shape_umap.csv'
 import joint from './data/joint_umap.csv'
+import test from './data/color_umap_test.csv'
 
 import atlas from './img/atlas.png'
+import { colors } from '@mui/material'
+import { AiOutlineConsoleSql } from 'react-icons/ai'
 
+
+const texture = new THREE.TextureLoader().load( atlas )
+texture.wrapS = THREE.RepeatWrapping;
+texture.wrapT = THREE.RepeatWrapping;
+
+const material = new THREE.PointsMaterial({ 
+	map: texture,
+	size: 0.07,
+	transparent: true, 
+	alphaTest: 0.5,
+})
+
+const indexParsChunk = [
+	'attribute float vIndex;',
+	'varying float texIndex;',
+	'#include <common>'
+].join( '\n' );
+
+const indexChunk = [
+	'#include <begin_vertex>',
+	'\ttexIndex = vIndex;'
+].join( '\n' );
+
+const fragmentParsChunk = [
+	'varying float texIndex;',
+	'#include <common>'
+].join( '\n' );
+
+const offsetChunk = `
+	#if defined( USE_MAP ) || defined( USE_ALPHAMAP )
+		vec2 uv = ( uvTransform * vec3( gl_PointCoord.x, 1.0 - gl_PointCoord.y, 1 ) ).xy;
+	#endif
+
+	#ifdef USE_MAP
+		float offsetX = mod(texIndex,150.) / 150.;
+		float offsetY = -floor(texIndex/150.) / 150. - 1./150.;
+		diffuseColor *= texture2D( map, uv/150. + vec2(offsetX,offsetY) );
+	#endif
+
+	#ifdef USE_ALPHAMAP
+		diffuseColor.a *= texture2D( alphaMap, uv ).g;
+	#endif
+`
+
+material.onBeforeCompile = function ( shader ) {
+
+	shader.vertexShader = shader.vertexShader
+		.replace( '#include <common>', indexParsChunk )
+		.replace( '#include <begin_vertex>', indexChunk );
+
+	shader.fragmentShader = shader.fragmentShader
+		.replace( '#include <common>', fragmentParsChunk )
+		.replace( '#include <map_particle_fragment>', offsetChunk );
+
+};
 
 export async function MakeCloud({ shoeType }) {
 	
-	const ref = null
-
   const geometry = new THREE.BufferGeometry()
-  const texture = new THREE.TextureLoader().load( atlas )
-  texture.wrapS = THREE.RepeatWrapping;
-  texture.wrapT = THREE.RepeatWrapping;
 
   const positions = []
   const texIndices = []
 
   const shoeData = 
-    shoeType == "color" ? color : 
-    shoeType == "shape" ? shape :
-    shoeType == "joint" ? joint : null
+    shoeType === "color" ? color : 
+    shoeType === "shape" ? shape :
+    shoeType === "joint" ? joint : 
+		shoeType === "test" ? test : null 
 
   const data = await d3.csv(shoeData)
 
   for (let i=0;i<data.length;i++) {
     let p = data[i]
-    positions.push( p.x, p.y, p.z )
-    texIndices.push( i )
+		if (p.colorful == 'True') {
+			positions.push( p.x, p.y, p.z )
+			texIndices.push( p.index )
+		}
   }
 
   geometry.setAttribute( 'position', new THREE.Float32BufferAttribute( positions, 3 ) )
   geometry.setAttribute( 'vIndex', new THREE.Float32BufferAttribute( texIndices, 1 ) )
 
-  const material = new THREE.PointsMaterial({ 
-    map: texture,
-    size: 0.2,
-    transparent: true, 
-    alphaTest: 0.5,
-  })
-
-  const indexParsChunk = [
-    'attribute float vIndex;',
-    'varying float texIndex;',
-    '#include <common>'
-  ].join( '\n' );
-
-  const indexChunk = [
-    '#include <begin_vertex>',
-    '\ttexIndex = vIndex;'
-  ].join( '\n' );
-
-  const fragmentParsChunk = [
-    'varying float texIndex;',
-    '#include <common>'
-  ].join( '\n' );
-
-  const offsetChunk = `
-    #if defined( USE_MAP ) || defined( USE_ALPHAMAP )
-      vec2 uv = ( uvTransform * vec3( gl_PointCoord.x, 1.0 - gl_PointCoord.y, 1 ) ).xy;
-    #endif
-
-    #ifdef USE_MAP
-      float offsetX = mod(texIndex,150.) / 150.;
-      float offsetY = -floor(texIndex/150.) / 150. - 1./150.;
-      diffuseColor *= texture2D( map, uv/150. + vec2(offsetX,offsetY) );
-    #endif
-
-    #ifdef USE_ALPHAMAP
-      diffuseColor.a *= texture2D( alphaMap, uv ).g;
-    #endif
-  `
-
-  material.onBeforeCompile = function ( shader ) {
-
-    shader.vertexShader = shader.vertexShader
-      .replace( '#include <common>', indexParsChunk )
-      .replace( '#include <begin_vertex>', indexChunk );
-
-    shader.fragmentShader = shader.fragmentShader
-      .replace( '#include <common>', fragmentParsChunk )
-      .replace( '#include <map_particle_fragment>', offsetChunk );
-
-  };
-
   return {geo: geometry, mat: material}
+}
+
+
+export async function MakeCloudDynamic({ shoeType }) {
+	
+  const shoeData = 
+    shoeType === "color" ? color : 
+    shoeType === "shape" ? shape :
+    shoeType === "joint" ? joint : 
+		shoeType === "test" ? test : null 
+
+  const data = await d3.csv(shoeData)
+	const years = {};
+
+
+  for (let i=0;i<data.length;i++) {
+    let p = data[i]
+		if (p.year != 'N/A') {
+			if (p.year in years) {
+				years[p.year].positions.push( p.x, p.y, p.z )
+				years[p.year].texIndices.push( p.index )
+			} else {
+				years[p.year] = {
+					positions: [ p.x, p.y, p.z],
+					texIndices: [ p.index ]
+				}
+			}
+			
+		}
+  }
+
+	let temporalMesh = {}
+
+	for (let i=0;i<Object.keys(years).length;i++) {
+		let year = Object.keys(years)[i]
+		let geometry = new THREE.BufferGeometry()
+		let texture = new THREE.TextureLoader().load( atlas )
+		texture.wrapS = THREE.RepeatWrapping;
+		texture.wrapT = THREE.RepeatWrapping;
+
+		geometry.setAttribute( 'position', new THREE.Float32BufferAttribute( years[year].positions, 3 ) )
+  	geometry.setAttribute( 'vIndex', new THREE.Float32BufferAttribute( years[year].texIndices, 1 ) )
+
+		temporalMesh[year] = {
+			geo: geometry,
+			mat: material
+		}
+	}
+
+  return temporalMesh
 }
